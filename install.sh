5f8f46fbd9e62d9b418c56ba50c21c5a707819ab
Moritz Meier <moritzmeier.mail@gmail.com>
2022-06-04 20:02:59 +0200

    wip

--- install.sh
+++ install.sh
@@ -20,10 +20,7 @@ if [ ! -z ${_BUILD_ARG_JLINK_TOOLS} ]; then
 	echo "Installing JLink tools $version"
 
 	apt update -y && apt install -y curl
-	curl -s -X POST \
-		-d "accept_license_agreement=accepted&submit=Download+software" \
-		-o "$HOME/jlink.deb" \
-		"https://www.segger.com/downloads/jlink/JLink_Linux_$version.deb"
+	curl -s -X POST -d "accept_license_agreement=accepted&submit=Download+software" -o "$HOME/jlink.deb" "https://www.segger.com/downloads/jlink/JLink_Linux_$version.deb"
 
 	apt install -fy "$HOME/jlink.deb"
 fi
