ed7b267572fe56a6aa6a0f197e8b46de0055bf99
Moritz Meier <moritzmeier.mail@gmail.com>
2022-06-04 19:47:54 +0200

    Add feature jlink_tools

--- install.sh
+++ install.sh
@@ -13,3 +13,13 @@ if [ ! -z ${_BUILD_ARG_TESTFEATURE1} ]; then
 	text=${_BUILD_ARG_TESTFEATURE1_TEXT:-undefined}
 	echo "$text" >> "/home/vscode/testfeature1.txt"
 fi
+
+if [ ! -z ${_BUILD_ARG_JLINK_TOOLS} ]; then
+
+	version=$_BUILD_ARG_JLINK_TOOLS_VERSION
+	echo "Installing JLink tools $version"
+
+	apt update -y && apt install -y curl
+	curl -X POST -d "accept_license_agreement=accepted&submit=Download+software" -o "/home/vscode/jlink.deb" "https://www.segger.com/downloads/jlink/JLink_Linux_$version.deb"
+
+fi
